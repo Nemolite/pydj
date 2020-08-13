faf14114ecc86a1932d538a44f1911aa0d9f91c5
Nemolite <g25092011@mail.ru>
2020-08-13 20:43:38 +0300

    step2fix

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dkxp00$%p*5q3rc&vp3fx0n76b6*yrp_w3&u5p1qvh*#ce32ar'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
